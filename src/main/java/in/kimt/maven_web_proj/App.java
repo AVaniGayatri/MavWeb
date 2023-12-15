4e5ef8d29b805ca1f029d1b72a7e92b0c152b30d
AVaniGayatri <avg0828gmail.com>
2023-12-15 14:57:42 +0530

    Demo of web

---
+++ src/main/java/in/kimt/maven_web_proj/App.java
@@ -0,0 +1,13 @@
+package in.kimt.maven_web_proj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
